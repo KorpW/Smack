09765a2b079c770ad936f14af1e336c33e43445b
Magnus Wærstad <mwarstad@gmail.com>
2018-12-19 21:54:35 +0100

    Starting project

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Magnus Eri Wærstad on 19/12/2018.
+//  Copyright © 2018 Magnus Eri Wærstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
